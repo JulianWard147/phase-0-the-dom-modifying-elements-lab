01619bfdef2b76f87f6e4f33eb6c930b1e17c374
Julian Ward <wardjulianm@gmail.com>
2023-03-01 10:15:45 -0700

    victory

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code here!
+
+// Remove main
+
+document.querySelector('main').remove()
+
+// add 'newHeader' that points to an <h1> node
+
+const newHeader = document.createElement('h1')
+
+
+// 'newHeader' id = 'victory'
+
+newHeader.setAttribute('id', 'victory')
+
+// 'newHeader' text "Julian is the champion"
+
+newHeader.textContent = "Julian is the champion"
+
+document.body.append(newHeader)
+
